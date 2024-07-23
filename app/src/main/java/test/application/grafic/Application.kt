463a17289284f4bae18290875b0980c763eeb2af
Doniyor <rdonik96@gmail.com>
2024-07-23 11:12:33 +0500

    - clear up

--- app/src/main/java/test/application/grafic/Application.kt
+++ app/src/main/java/test/application/grafic/Application.kt
@@ -1,20 +1,8 @@
 package test.application.grafic
 
 import android.app.Application
-import android.content.Context
-
 import dagger.hilt.android.HiltAndroidApp
 
+
 @HiltAndroidApp
-class Application : Application() {
-
-    override fun attachBaseContext(base: Context) {
-
-        super.attachBaseContext(base)
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-    }
-
-}
+class Application : Application()
--- app/src/main/java/test/application/grafic/domain/GetCoordinatesUseCase.kt
+++ app/src/main/java/test/application/grafic/domain/GetCoordinatesUseCase.kt
@@ -4,18 +4,18 @@ import kotlinx.coroutines.CoroutineDispatcher
 import test.application.grafic.base.UseCase
 import test.application.grafic.data.point.PointResponse
 import test.application.grafic.di.IoDispatcher
-import test.application.grafic.network.ErrorApiResult
 import test.application.grafic.network.NetworkResult
-import test.application.grafic.network.SuccessApiResult
 import test.application.grafic.network.repositorys.PointsRepository
 import test.application.grafic.shared.result.getResult
 import javax.inject.Inject
 
 class GetCoordinatesUseCase @Inject constructor(
-    private val pointsRepository: PointsRepository,
-    @IoDispatcher ioDispatcher: CoroutineDispatcher
+    @IoDispatcher ioDispatcher: CoroutineDispatcher,
+    private val pointsRepository: PointsRepository
 ) : UseCase<Int, NetworkResult<PointResponse>>(ioDispatcher) {
+
     override suspend fun performAction(parameters: Int): NetworkResult<PointResponse> {
         return getResult { pointsRepository.checkPoints(parameters) }
     }
+
 }
